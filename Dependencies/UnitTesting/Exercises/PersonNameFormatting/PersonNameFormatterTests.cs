c2670cca6b89eae2c55f420171cf905a335ecf01
olafthielke <olaf.thielke@gmail.com>
2024-02-27 13:00:45 +1300

    Remove obsolete UnitTests project.

---
+++ Dependencies/UnitTesting/Exercises/PersonNameFormatting/PersonNameFormatterTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Dependencies.UnitTesting.Exercises.PersonNameFormatting
+{
+    /// TODO: Write a class to format a person's first and last names into '{firstname}, {lastname}' format.
+    /// E.g. (Fred, Flinstone) becomes 'Flintstone, Fred'
+    /// (Barney, Rubble) becomes 'Rubble, Barney'
+    // Suggestion: Feel free to try TDD.    
+
+    public class PersonNameFormatterTests
+    {
+        [Fact]
+        public void FormatName_Returns_Formatted_Name()
+        {
+            // TODO: Write test
+            
+        }
+    }
+}
